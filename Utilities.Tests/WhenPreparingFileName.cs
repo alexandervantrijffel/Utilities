1a302f56d30af4f9c77c3925d366c77323371693
Alexander van Trijffel <lexmail@gmail.com>
2015-02-01 21:18:28 +0100

    Fixed RemoveIllegalFileNameCharacters

---
+++ Utilities.Tests/WhenPreparingFileName.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FluentAssertions;
+using Xunit;
+using Structura.SharedComponents.Utilities;
+
+namespace Structura.SharedComponents.Utilities.Tests
+{
+    public class WhenPreparingFileName
+    {
+        [Fact]
+        public void AllIllegalCharactersAreRemoved()
+        {
+            // Arrange
+            var input = "<t>h:i\"s/\\|?*";
+
+            // Act
+            var result = input.RemoveIllegalFileNameCharacters();
+            
+            // Assert
+            result.Should().Be("this", "not expected result string: " + result);
+        }
+    }
+}
---
+++ Utilities/DataAnnotationsValidationResult.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace Structura.Ptb.Utilities.Validation
+{
+    public class ValidationResultEx
+    {
+        public bool IsValid { get; set; }
+        public IEnumerable<ValidationResult> ValidationMessages { get; set; }
+
+        public string ValidationMessagesToString()
+        {
+            if (ValidationMessages == null)
+                return string.Empty;
+
+            var sb = new StringBuilder();
+            foreach (ValidationResult validationResult in ValidationMessages)
+            {
+                sb.Append(validationResult.ErrorMessage);
+                sb.Append(" ");
+            }
+            return sb.ToString();
+        }
+    }
+}
