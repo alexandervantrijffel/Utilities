cab5e01bba54e15ba23bb533ae8ada327584449c
Alexander van Trijffel <lexmail@gmail.com>
2015-02-08 17:41:05 +0100

    updated configuration

---
+++ Utilities/DataAnnotationsValidatorEx.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace Structura.Ptb.Utilities.Validation
+{
+    public class DataAnnotationsValidatorEx
+    {
+        public static ValidationResultEx ValidateObject(object objectToValidate)
+        {
+            var context = new ValidationContext(objectToValidate, null, null);
+            var messages = new List<ValidationResult>();
+            var result = new ValidationResultEx
+                {
+                    IsValid = Validator.TryValidateObject(objectToValidate, context, messages),
+                    ValidationMessages = messages
+                };
+            return result;
+        }
+    }
+}
