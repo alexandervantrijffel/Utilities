aa14e805177999c13047704997a96e738a18ef82
Alexander van Trijffel <lexmail@gmail.com>
2015-04-19 10:13:59 +0200

    Added notfoundexception

---
+++ Utilities/NotFoundException.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace Structura.SharedComponents.Utilities
+{
+    /// <summary>
+    /// Thrown when item cannot be found in the data repository. 
+    /// Return a HTTP 404 to the client.
+    /// </summary>
+    [Serializable]
+    public class NotFoundException : Exception
+    {
+        //
+        // For guidelines regarding the creation of new exception types, see
+        //    http://msdn.microsoft.com/library/default.asp?url=/library/en-us/cpgenref/html/cpconerrorraisinghandlingguidelines.asp
+        // and
+        //    http://msdn.microsoft.com/library/default.asp?url=/library/en-us/dncscol/html/csharp07192001.asp
+        //
+
+        public NotFoundException()
+        {
+        }
+
+        public NotFoundException(string message)
+            : base(message)
+        {
+        }
+
+        public NotFoundException(string message, Exception inner)
+            : base(message, inner)
+        {
+        }
+
+        protected NotFoundException(
+            SerializationInfo info,
+            StreamingContext context)
+            : base(info, context)
+        {
+        }
+    }
+}
