7e5bdd4abd05eab5b174ce7140127e0f2bfc6000
Alexander van Trijffel <lexmail@gmail.com>
2016-02-09 15:52:14 +0100

    Added ListExtensions, updated log4net

---
+++ Utilities/ListExtensions.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Structura.SharedComponents.Utilities
+{
+    public static class ListExtensions
+    {
+        public static bool TryUpdateExisting<T>(this IList<T> list, Func<T, bool> selector, T replacement)
+        {
+            var existing = list.FirstOrDefault(selector);
+            if (existing != null)
+            {
+                list[list.IndexOf(existing)] = replacement;
+            }
+            return existing != null;
+        }
+    }
+}
