b2e57cea646c011256d25644022d542bd0ff4afb
Alexander van Trijffel <lexmail@gmail.com>
2016-02-26 13:57:55 +0100

    Added currentdirectory

---
+++ Utilities.Tests/CurrentDirectoryForUnitTests.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.IO;
+using System.Reflection;
+
+namespace Structura.SharedComponents.Utilities.Tests
+{
+    public static class CurrentDirectoryForUnitTests
+    {
+        public static string DirectoryName => new Uri(Path.GetDirectoryName(Assembly.GetExecutingAssembly().CodeBase)).LocalPath;
+    }
+}
