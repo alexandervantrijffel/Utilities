214654359eb5074350a6d66a2463f4a2001c0adb
Alexander van Trijffel <alexander@structura.ws>
2016-05-20 16:45:04 +0200

    Performance tester helper

---
+++ Utilities.Tests/PerformanceTester.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace Structura.Shared.Utilities.Tests
+{
+	public class PerformanceTester
+	{
+		public TimeSpan Elapsed { get; }
+
+		public PerformanceTester(Action method)
+		{
+			var sw = new Stopwatch();
+			sw.Start();
+			method();
+			sw.Stop();
+			Elapsed = sw.Elapsed;
+			Debug.WriteLine($"Method completed in {Elapsed.Milliseconds} ms.");
+		}
+	}
+}
