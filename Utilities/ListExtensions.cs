59d89b0c41449670be070770ba8a8cd0a58291e5
Alexander van Trijffel <lexmail@gmail.com>
2016-06-08 10:08:16 +0200

    AddOrUpdate on IList

--- Utilities/ListExtensions.cs
+++ Utilities/ListExtensions.cs
@@ -6,6 +6,18 @@ namespace Structura.SharedComponents.Utilities
 {
     public static class ListExtensions
     {
+        public static void AddOrUpdate<T>(this IList<T> coll, T item, Func<T, T, bool> equalityComparer)
+        {
+            for (int i = 0; i < coll.Count; i++)
+            {
+                if (equalityComparer(coll[i], item))
+                {
+                    coll[i] = item;
+                }
+            }
+            coll.Add(item);
+        }
+
         public static bool TryUpdateExisting<T>(this IList<T> list, Func<T, bool> selector, T replacement)
         {
             var existing = list.FirstOrDefault(selector);
@@ -15,5 +27,7 @@ namespace Structura.SharedComponents.Utilities
             }
             return existing != null;
         }
+
+        
     }
 }
