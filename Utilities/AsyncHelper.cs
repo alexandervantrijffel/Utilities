9adee792e41ea776383386dfe8490261b7ca30a6
Alexander van Trijffel <lexmail@gmail.com>
2018-03-19 09:58:00 +0100

    Update AsyncHelper.cs

--- Utilities/AsyncHelper.cs
+++ Utilities/AsyncHelper.cs
@@ -4,36 +4,78 @@ using System.Threading.Tasks;
 
 namespace Structura.Shared.Utilities
 {
-    /// <summary>
-    /// Helper class for running async methods synchronously
-    /// </summary>
-    public static class AsyncHelper
+    public static class AsyncHelpers
     {
-        private static readonly TaskFactory _myTaskFactory = new
-            TaskFactory(CancellationToken.None,
-                TaskCreationOptions.None,
-                TaskContinuationOptions.None,
-                TaskScheduler.Default);
-
-        public static TResult RunSync<TResult>(Func<Task<TResult>> func)
+        /// <summary>
+        /// Execute's an async Task<T> method which has a void return value synchronously
+        /// </summary>
+        /// <param name="task">Task<T> method to execute</param>
+        public static void RunSync(Func<Task> task)
         {
-            return AsyncHelper._myTaskFactory
-                .StartNew<Task<TResult>>(func)
-                .Unwrap<TResult>()
-                .GetAwaiter()
-                .GetResult();
+            var oldContext = SynchronizationContext.Current;
+            var synch = new ExclusiveSynchronizationContext();
+            SynchronizationContext.SetSynchronizationContext(synch);
+            synch.Post(async _ =>
+            {
+                try
+                {
+                    await task();
+                }
+                catch (Exception e)
+                {
+                    synch.InnerException = e;
+                    throw;
+                }
+                finally
+                {
+                    synch.EndMessageLoop();
+                }
+            }, null);
+            synch.BeginMessageLoop();
+
+            SynchronizationContext.SetSynchronizationContext(oldContext);
         }
 
-        public static void RunSync(Func<Task> func)
+        /// <summary>
+        /// Execute's an async Task<T> method which has a T return type synchronously
+        /// </summary>
+        /// <typeparam name="T">Return Type</typeparam>
+        /// <param name="task">Task<T> method to execute</param>
+        /// <returns></returns>
+        public static T RunSync<T>(Func<Task<T>> task)
         {
-            AsyncHelper._myTaskFactory
-                .StartNew<Task>(func)
-                .Unwrap()
-                .GetAwaiter()
-                .GetResult();
+            var oldContext = SynchronizationContext.Current;
+            var synch = new ExclusiveSynchronizationContext();
+            SynchronizationContext.SetSynchronizationContext(synch);
+            T ret = default(T);
+            synch.Post(async _ =>
+            {
+                try
+                {
+                    ret = await task();
+                }
+                catch (Exception e)
+                {
+                    synch.InnerException = e;
+                    throw;
+                }
+                finally
+                {
+                    synch.EndMessageLoop();
+                }
+            }, null);
+            synch.BeginMessageLoop();
+            SynchronizationContext.SetSynchronizationContext(oldContext);
+            return ret;
         }
 
-        public static async Task<bool> RunWithTimeout(Task task, TimeSpan timeout)
+        /// <summary>
+        /// Execute an async Task with a specified timeout, if the timeout is hit a false will be returened
+        /// </summary>
+        /// <param name="task">The task to be ran</param>
+        /// <param name="timeout">The timespan the task execution might unltimatly take</param>
+        /// <returns>A boolean, true if execution was in time, false if not</returns>
+        public static async Task<bool> RunWithTimeout(this Task task, TimeSpan timeout)
         {
             var result = await Task.WhenAny(task, Task.Delay(timeout));
             var taskCompleted = result == task;
@@ -41,12 +83,77 @@ namespace Structura.Shared.Utilities
             return taskCompleted;
         }
 
-        public static async Task<(bool)> RunWithTimeout<TTaskReturnVal>(Task<TTaskReturnVal> task, TimeSpan timeout)
+        /// <summary>
+        /// Execute an async Task with a specified timeout, if the timeout is hit a false will be returened
+        /// </summary>
+        /// <param name="task">The task to be ran</param>
+        /// <param name="timeout">The timespan the task execution might unltimatly take</param>
+        /// <returns>A boolean, true if execution was in time, false if not, and the return value of the task</returns>
+        public static async Task<(bool TaskCompleted, Task<TTaskReturnVal> TaskResult)> RunWithTimeout<TTaskReturnVal>(this Task<TTaskReturnVal> task, TimeSpan timeout)
         {
             var result = await Task.WhenAny(task, Task.Delay(timeout));
             var taskCompleted = result == task;
             if (taskCompleted) await task;
-            return (taskCompleted);
+            return (taskCompleted, task);
+        }
+
+        class ExclusiveSynchronizationContext : SynchronizationContext
+        {
+            bool _done;
+            public Exception InnerException { get; set; }
+            readonly AutoResetEvent _workItemsWaiting = new AutoResetEvent(false);
+            readonly Queue<Tuple<SendOrPostCallback, object>> _items = new Queue<Tuple<SendOrPostCallback, object>>();
+
+            public override void Send(SendOrPostCallback d, object state)
+            {
+                throw new NotSupportedException("We cannot send to our same thread");
+            }
+
+            public override void Post(SendOrPostCallback d, object state)
+            {
+                lock (_items)
+                {
+                    _items.Enqueue(Tuple.Create(d, state));
+                }
+                _workItemsWaiting.Set();
+            }
+
+            public void EndMessageLoop()
+            {
+                Post(_ => _done = true, null);
+            }
+
+            public void BeginMessageLoop()
+            {
+                while (!_done)
+                {
+                    Tuple<SendOrPostCallback, object> task = null;
+                    lock (_items)
+                    {
+                        if (_items.Count > 0)
+                        {
+                            task = _items.Dequeue();
+                        }
+                    }
+                    if (task != null)
+                    {
+                        task.Item1(task.Item2);
+                        if (InnerException != null) // the method threw an exeption
+                        {
+                            throw new AggregateException("AsyncHelpers.Run method threw an exception.", InnerException);
+                        }
+                    }
+                    else
+                    {
+                        _workItemsWaiting.WaitOne();
+                    }
+                }
+            }
+
+            public override SynchronizationContext CreateCopy()
+            {
+                return this;
+            }
         }
     }
 }
